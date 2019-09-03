8e49d25ed71d56a18b3b6071ae6840555b3ec612
Maxim Krouk <m.krouk@vironit.co.uk>
2019-09-03 12:39:21 +0300

    Example [update]: Example added.

--- Example/SweetUI/ViewController.swift
+++ Example/SweetUI/ViewController.swift
@@ -6,13 +6,23 @@
 //  Copyright (c) 2019 Maxim Krouk. All rights reserved.
 //
 
-import UIKit
+import SweetUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.ui.add {
+            UIView().ui
+                .background(color: .red)
+                .size(.square(length: 100))
+                .center(view.center)
+                .cornerRadius(10)
+                .alpha(0)
+                .animate(.fadeIn(5, curve: .easeInOut))
+                .view
+        }
     }
 
     override func didReceiveMemoryWarning() {
